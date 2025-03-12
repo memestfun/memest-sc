be8c5498db34fe1b25d189bbeeb40ded855edd0d
goni098 <mtheanh2910@gmail.com>
2025-03-12 14:16:30 +0700

    publish to testnet

--- client/setup.ts
+++ client/setup.ts
@@ -5,8 +5,8 @@ export const goni_secret_key = ""
 export const gonisbaby_secret_key = ""
 
 export const package_id =
-	"0x0c2a0046b94e660cf95ed3711814bb1495ae4f96bdd1f016da83ae6e98f3c390"
+	"0xf1c84f3b63a233ad9706ced7fda88c1dadc5748bbb7f8cfc21dfe87274a314a3"
 
 export const client = new SuiClient({
-	url: getFullnodeUrl("localnet")
+	url: getFullnodeUrl("testnet")
 })
